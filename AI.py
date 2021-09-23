ce752d37c57f1bbb9cdd7d8126823a4f341a60dc
RolandGulbinovic <roland.gulbinovicvu@gmail.com>
2021-09-23 19:07:00 +0300

    made so that it caluclates the sums

--- AI.py
+++ AI.py
@@ -1,3 +1,39 @@
-print("Hello World")
-print("testing still cause im dumb")
-other way around test
+import numpy as np
+
+arr_x = np.array([-0.2, 0.5])
+
+
+def bias(a):
+    if a > 0:
+        y = 1
+    else:
+        y = 0
+    return y
+
+
+arr_x_1 = np.array([])
+for j in range(0, 11):
+    i = arr_x[0]
+    j = j/10
+    arr_x_1 = np.append(arr_x_1, i*j)
+
+arr_x_2 = np.array([])
+for j in range(0, 11):
+    i = arr_x[1]
+    j = j/10
+    arr_x_2 = np.append(arr_x_2, i*j)
+
+arr_x_1 = arr_x_1.reshape(11, 1)
+
+
+arr_x_2 = arr_x_2.reshape(11, 1)
+
+
+for i in arr_x_1:
+    for j in arr_x_2:
+        temp = str(i) + " + " + str(j)
+        print(temp, i*j, sep=" = ")
+
+
+# y = bias(a)
+# print(y)
